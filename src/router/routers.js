0ae76f6e8ec731938f1b62c77c8fb007ac1c2dda
wangzeqing <wangzeqing@youzhengkeji.com>
2019-03-27 19:30:21 +0800

    'add_meetting'

--- src/router/routers.js
+++ src/router/routers.js
@@ -234,6 +234,14 @@ const routerMap = [
     component: r => require.ensure([], () => r(require('@/page/comm/onesInfo.vue')))
   },
   {
+    path: '/meetList',
+    name: 'meetList',
+    meta: {
+      requireAuth: true
+    },
+    component: r => require.ensure([], () => r(require('@/page/meetList/meetList.vue')))
+  },
+  {
     path: '/caseInfo',
     meta: {
       requireAuth: true
@@ -316,7 +324,7 @@ export const getRouter = (obj) => {
   let _a = obj.menu
   let _r = []
   let _l = 0
-  _a.push('/onesInfo', '/caseInfo', '/idenInfo', '/paymentInfo')
+  _a.push('/onesInfo', '/caseInfo', '/idenInfo', '/paymentInfo', '/meetList')
   _r.push({
     path: '/',
     redirect: _a[0]
