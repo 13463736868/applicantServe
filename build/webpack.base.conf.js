f346d021e8159f3505a02fef32126831df42a95a
wangzeqing <wangzeqing@youzhengkeji.com>
2019-11-16 14:26:18 +0800

    '111'

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -37,6 +37,7 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      '_static': resolve('static'),
       '../../static': resolve('static'),
       '../../../static': resolve('static')
     }
