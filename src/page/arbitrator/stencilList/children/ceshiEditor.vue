558c531fcdbe4f18b14b4b855bd561a72bc48d12
wangzeqing <wangzeqing@youzhengkeji.com>
2020-04-30 14:53:31 +0800

    'ueditor-'

--- src/page/arbitrator/stencilList/children/ceshiEditor.vue
+++ src/page/arbitrator/stencilList/children/ceshiEditor.vue
@@ -357,7 +357,7 @@
 <script>
 import axios from 'axios'
 import '../../../static/ueditor/ueditor.config.js'
-import '../../../static/ueditor/ueditor.all.min.js'
+import '../../../static/ueditor/ueditor.all.js'
 import '../../../static/ueditor/lang/zh-cn/zh-cn.js'
 import '../../../static/ueditor/ueditor.parse.min.js'
 import alertBtnInfo from '@/components/common/alertBtnInfo'
